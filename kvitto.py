9bbd70e265a7c458411ce6a0d006d3e13c12696b
MrPota2 <adrian.milvang@gmail.com>
2023-06-22 14:42:06 +0200

    Insert statements

--- kvitto.py
+++ kvitto.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
+# Create database
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
     try:
@@ -21,7 +22,62 @@ def create_table(conn, create_table_sql):
         print(e)
 
 
-def main():
+# Insert into database
+def create_user(conn, user):
+    """
+    Create a new user into the user table
+    :param conn:
+    :param user:
+    :return: user id
+    """
+    sql = ''' INSERT INTO user(name)
+              VALUES(?) '''
+    cur = conn.cursor()
+    cur.execute(sql, user)
+    conn.commit()
+    return cur.lastrowid
+
+def create_receipt(conn, timestamp, store, victim):
+    """
+    Create a new receipt into the receipt table
+    :param conn:
+    :param receipt:
+    :return: receipt id
+    """
+    sql = ''' INSERT INTO receipt(timestamp,store,victim)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, (timestamp,store,victim))
+    conn.commit()
+    return cur.lastrowid
+
+def create_item(conn, timestamp, name, price, payor):
+    """
+    Create a new item into the item table
+    :param conn:
+    :param item:
+    """
+    sql = ''' INSERT INTO item(timestamp,name,price)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, (timestamp,name,price))
+    conn.commit()
+
+def create_status(conn, timestamp, user, status):
+    """
+    Create a new status into the status table
+    :param conn:
+    :param status:
+    """
+    sql = ''' INSERT INTO status(timestamp,user,status)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, (timestamp,user,status))
+    conn.commit()
+
+
+
+def create_db():
     database = r"kvitto.db"
 
     ### SQL statements ###
@@ -43,6 +99,7 @@ def main():
                                         timestamp text,
                                         name text,
                                         price real NOT NULL,
+                                        payor text,
                                         PRIMARY KEY (timestamp, name),
                                         FOREIGN KEY (timestamp) REFERENCES receipt (timestamp)
                                     ); """
@@ -68,4 +125,5 @@ def main():
     else:
         print("Error! cannot create the database connection.")
 
-main()
+def main():
+    create_db()
